740fdf49de77a72156c365ce64b32e61a957168d
vahan <4753637+Vahanerevan@users.noreply.github.com>
2025-05-08 18:20:45 +0400

    remove env conf

--- internal/web/middleware/middleware.go
+++ internal/web/middleware/middleware.go
@@ -21,15 +21,29 @@ func NewAuthMiddleware(cfg *config.Config) *AuthMiddleware {
 
 func (a *AuthMiddleware) Handle(c *fiber.Ctx) error {
 	token := c.Get("Authorization")
+	service := c.Get("X-Service")
 	if token == "" {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"error": "Authorization header missing",
 		})
 	}
+	var url string
+	switch service {
+	case "Affiliate":
+		url = "http://affiliate-new/api/check-auth-user"
+		break
+	case "Control":
+		url = "http://control-api/api/check-auth-user"
+		break
+	default:
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "X-Service header missing",
+		})
+	}
 
 	resp, err := a.client.R().
 		SetHeader("Authorization", "Bearer "+token).
-		Post("http://control-api/api/check-auth-user")
+		Post(url)
 
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
